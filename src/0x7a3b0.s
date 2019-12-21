b582095e07c87aec388867fed438140f7dacd4f3
Jonathan Thomas <jonathan@opalise.co.uk>
2019-12-21 16:39:36 +0000

    further refine masking

--- src/0x7a3b0.s
+++ src/0x7a3b0.s
@@ -1,5 +1,13 @@
     ORG $7a3b0
 
+    ; a1 is source address
+    ; a1 is destination address
+    ; d3 is the lines to be drawn
+    ; d4 is number of 16 pixel blocks to be drawn (= 8 words)
+    ; - so if d4 = 1, we want to draw 16 pixels = 4 words = 8 bytes
+    ; d6 is destination bytes to skip after each line
+    ; d7 is source bytes to skip after each line
+
     add.w #8,d6
     add.w #10,d7
 
@@ -11,23 +19,18 @@
     move.w #8,($ffff8a2e).w             ; dest x increment
     move.w d6,($ffff8a30).w             ; dest y increment
     move.w d4,($ffff8a36).w             ; xcount = number of 16 pixel blocks (once pass per bitplane)
+    move.w #$0201,($ffff8a3a).w         ; hop/op
+    ;move.b #$00,($ffff8a3d).w           ; not sure if this is required?
 
-    ; a1 is source address
-    ; a1 is destination address
-    ; d3 is the lines to be drawn
-    ; d4 is number of 16 pixel blocks to be drawn (= 8 words)
-    ; - so if d4 = 1, we want to draw 16 pixels = 4 words = 8 bytes
-    ; d6 is destination bytes to skip after each line
-    ; d7 is source bytes to skip after each line
+    ; we are now free to use d7, d6 and d4 for our own purposes
+    move.l #$c0,d7
 
     rept 4
 
     move.w d3,($ffff8a38).w             ; ycount
     move.l a0,($ffff8a24).w             ; set source address
     move.l a1,($ffff8a32).w             ; set destination
-    move.w #$0201,($ffff8a3a).w         ; hop/op
-    move.b #$00,($ffff8a3d).w           ; start
-    move.b #$c0,($ffff8a3c).w           ; start
+    move.b d7,($ffff8a3c).w             ; start
 
     addq.l #2,a1
 
