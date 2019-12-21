19f809673c5a9e24350035b4071d1783fa73ff1d
Jonathan Thomas <jonathan@opalise.co.uk>
2019-12-21 16:06:41 +0000

    roadside scenery masks - work in progress

--- src/0x7a3b0.s
+++ src/0x7a3b0.s
@@ -1,9 +1,12 @@
     ORG $7a3b0
 
-    add.w d4,d4
-    add.w d4,d4
+    ;add.w d4,d4
+    ;add.w d4,d4
 
-    add.w #2,d6
+    add.w #8,d6
+
+    add.w d7,d7
+    add.w d7,d7
 
     ; a1 is source address
     ; a1 is destination address
@@ -13,20 +16,26 @@
     ; d6 is destination bytes to skip after each line
     ; d7 is source bytes to skip after each line
 
-    move.w #2,($ffff8a20).w             ; source x increment
-    move.w #2,($ffff8a22).w             ; source y increment
+    rept 2
+
+    addq.l #2,a1
+
+    move.w #10,($ffff8a20).w            ; source x increment
+    move.w #10,($ffff8a22).w             ; source y increment
     move.w #-1,($ffff8a28).w            ; endmask1
     move.w #-1,($ffff8a2a).w            ; endmask2
     move.w #-1,($ffff8a2c).w            ; endmask3
-    move.w #2,($ffff8a2e).w             ; dest x increment
+    move.w #8,($ffff8a2e).w             ; dest x increment
     move.w d6,($ffff8a30).w             ; dest y increment
-    move.w d4,($ffff8a36).w             ; xcount
+    move.w d4,($ffff8a36).w             ; xcount = number of 16 pixel blocks (once pass per bitplane)
     move.w d3,($ffff8a38).w             ; ycount
     move.l a0,($ffff8a24).w             ; set source address
     move.l a1,($ffff8a32).w             ; set destination
-    move.w #$0203,($ffff8a3a).w         ; hop/op
+    move.w #$0201,($ffff8a3a).w         ; hop/op
+    move.b #$00,($ffff8a3d).w           ; start
     move.b #$c0,($ffff8a3c).w           ; start
-    move.b #$cc,($ffff8a3d).w           ; start
+
+    endr
 
     rts
 
