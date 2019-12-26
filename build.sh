6e5459a04ad9b3b50bde6e0cfa2e135a22f97c7b
Jonathan Thomas <jonathan@opalise.co.uk>
2019-12-26 13:37:38 +0000

    update build script

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ vasmm68k_mot src/0x7a312.s -Fbin -o bin/0x7a312.bin
 vasmm68k_mot src/0x76690.s -Fbin -o bin/0x76690.bin
 vasmm68k_mot src/0x80000.s -Fbin -o bin/0x80000.bin
 
-echo "If the build process has succeeded, files named 0x80000.bin, 0x76690.bin and 0x7a3b0 will now be present in the bin directory"
+echo "If the build process has succeeded, files named 0x7a2c0.bin, 0x7a2dc.bin, 0x7a312.bin, 0x76690.bin and 0x80080000.bin will now be present in the bin directory"
