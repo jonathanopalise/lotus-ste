bc0247fe3d1842beced1ca14f381ce0d75871a8f
Jonathan Thomas <jonathan@opalise.co.uk>
2020-01-05 10:42:16 +0000

    road optimisations - change long operations to word operations

--- src/0x80000.s
+++ src/0x80000.s
@@ -14,15 +14,15 @@ initdrawroad:
     move.w d4,$76688
     move.w #8,(a5)+            ; source x increment 8a20
     move.w #-158,(a5)          ; source y increment 8a22
-    add.l #6,a5
+    addq.l #6,a5
     move.w #-1,(a5)+           ; endmask1 8a28
     move.w #-1,(a5)+           ; endmask2 8a2a
     move.w #-1,(a5)+           ; endmask3 8a2c
     move.w #8,(a5)+            ; dest x increment 8a2e
     move.w #-150,(a5)          ; dest y increment 8x30
-    add.l #6,a5
+    addq.l #6,a5
     move.w #20,(a5)           ; xcount 8a36
-    add.l #4,a5
+    addq.l #4,a5
     move.w #$0203,(a5)        ; hop/op 8a3a
     jmp $76672
 
@@ -34,20 +34,20 @@ drawroad:
 
     move.w #4,($ffff8a38).w            ; ycount
 
-    lsr.l #4,d0                        ; bring the road width value into a 0-255 range
-    and.l #$ff,d0                      ; bring the road width value into a 0-255 range
-    add.l d0,d0 
-    add.l d0,d0                        ; bring the road width value into a 0-1023 range with increments of 4
+    lsr.w #4,d0                        ; bring the road width value into a 0-255 range
+    and.w #$ff,d0                      ; bring the road width value into a 0-255 range
+    add.w d0,d0 
+    add.w d0,d0                        ; bring the road width value into a 0-1023 range with increments of 4
 
     lsr.w #5,d2                        ; d2 should contain something telling us about the road position of current line
     andi.w #$20,d2
     beq.s skipoffsetadd 
-    add.l #1024,d0
+    add.w #1024,d0
 
 skipoffsetadd:
 
     move.l #byte_offsets,a0
-    add d0,a0                          ; d0 is the offset to the pointer to the road width segment we want
+    add.l d0,a0                          ; d0 is the offset to the pointer to the road width segment we want
     move.l (a0),a0                     ; a0 now contains the pointer to the road graphics data offset for the current line
     move.l #gfx_data,a2
     add.l a2,a0                        ; a0 now contains memory location of central source
@@ -98,7 +98,7 @@ drawscenery:
     move.w #$0201,($ffff8a3a).w         ; hop/op: read from source, source & destination
 
     move.l a3,d0                        ; get desired xpos of scenery object
-    and.l #$f,d0                        ; convert to skew value for blitter
+    and.w #$f,d0                        ; convert to skew value for blitter
     move.w d0,d1
     beq.s nonfsr                        ; if skew is zero, we can't use nfsr
 
