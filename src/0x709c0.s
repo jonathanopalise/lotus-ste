109610b71de2672d9045ce229b04f22f000ca434
Jonathan Thomas <jonathan@opalise.co.uk>
2021-03-04 19:20:13 +0000

    playing of correct sound effects for event numbers

--- src/0x709c0.s
+++ src/0x709c0.s
@@ -5,12 +5,15 @@
 
     include generated/symbols_0x80000.inc
 
-    lea pcm_mission1,a0
-    move.l a0,sample2
-    move.l a0,sample2_mix
-    move.b #(8250/250),sample2_size
-    move.b #(8250/250),sample2_len
-    move.b 0,sample2_loop
+    lea sample_lookup,a0
+    moveq.l #0,d0
+    move.w $7097c,d0
+    asl.w #3,d0
+    add.l d0,a0
+    move.l (a0)+,sample2
+    move.b (a0),sample2_size
+    move.b (a0),sample2_len
+    move.b #0,sample2_loop
     move.b #255,sample2_playing
 
     movem.l (sp)+,d0-d7/a0-a6
