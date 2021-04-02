6802a99ea304d556e730ba8787c9b52482d207c9
Jonathan Thomas <jonathan@opalise.co.uk>
2021-04-03 00:09:10 +0100

    further optimise and refine

--- src/status_panel.s
+++ src/status_panel.s
@@ -11,34 +11,36 @@
     or.w      d1,-4(a1)
     endm
 
-    macro status_panel_blitter_blit
-    endm
-
 render_status_panel:
     lea $ffff8a20.w,a5       ; source x increment 8a20
     move.w #8,(a5)+          ; source x increment 8a20
     move.w #0,(a5)+          ; source y increment 8a22
     move.l a5,a6             ; back up source address 8a24
-    ;lea 10(a5),a5
     addq.l #4,a5
-    move.w #-1,(a5)+         ; endmask1 8a28
-    move.w #-1,(a5)+         ; endmask2 8a2a
-    move.w #-1,(a5)+         ; endmask2 8a2c
+    moveq.l #-1,d2
+    move.w d2,(a5)+         ; endmask1 8a28
+    move.w d2,(a5)+         ; endmask2 8a2a
+    move.w d2,(a5)+         ; endmask2 8a2c
+    moveq.l #1,d2
     move.w #8,(a5)+          ; dest x increment 8a2e
     move.w #152-16,(a5)+     ; dest y increment 8a30
     move.l a5,a2             ; backup destination address 8a32
     addq.l #4,a5
     move.w #4,(a5)+          ; xcount 8a36
     move.l a5,a4             ; a4 is now address of ycount (8a38)
-    addq.l #2,a5             ; a3 is now address of hop/op (8a3a)
-    move.w #$f,(a5)+         ; hop/op 8a3a, advance a3 to linenum 8a3c
-    ; a5 is now 8a3c
+    addq.l #2,a5             ; proceed to hop/op 8a3a
+    move.l a5,a3             ; copy hop/op 8a3a into a3 reg
+    addq.l #2,a5             ; a5 is now blitter control 8a3c
+    move.w #$c080,d3
 
 status_panel_line:
     ; first pass
     status_panel_cpu_blit
 
     adda.w    #$80,a1
+
+    ; this shouldn't be necessary - understand why
+    suba.w    #$8,a0
 
     ; transfer a0 to blitter source
     ; transfer a1 to blitter destination
@@ -50,7 +52,7 @@ status_panel_line:
     ; approx $5f lines
 
     ; set source & destination
-    move.w #$0201,($ffff8a3a).w ; source & destination
+    move.w #$0201,(a3) ; source & destination
 
     rept 3
     jsr drawstatusplane
@@ -59,7 +61,7 @@ status_panel_line:
     jsr drawstatusplane
 
     subq.l #6,a1                        ; move destination back to initial bitplane
-    move.w #$0207,($ffff8a3a).w         ; hop/op: read from source, source | destination
+    move.w #$0207,(a3)         ; hop/op: read from source, source | destination
 
     rept 2
     addq.l #2,a0                        ; move source to next bitplane
@@ -86,12 +88,13 @@ status_panel_line:
     rts
 
 drawstatusplane:
-    move.l a0,($ffff8a24).w    ; source
-    move.l a1,($ffff8a32).w    ; destination
+    move.l a0,(a6)    ; source
+    move.l a1,(a2)    ; destination
 
+    ; might be able to exploit the fact that there are empty lines in the status display
     rept 92
-    move.w #1,($ffff8a38).w             ; ycount
-    move.w #$c080,($ffff8a3c).w         ; control
+    move.w d2,(a4)             ; ycount
+    move.w d3,(a5)         ; control
     endr
     rts
 
