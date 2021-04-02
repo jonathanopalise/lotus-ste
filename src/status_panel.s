5e83f18ce5b1523feec0da9991666d591ff40316
Jonathan Thomas <jonathan@opalise.co.uk>
2021-04-02 20:48:17 +0100

    flatten duplicated code into macros

--- src/status_panel.s
+++ src/status_panel.s
@@ -1,52 +1,27 @@
+    macro status_panel_cpu_blit
+    move.w    (a0)+,d0       ; load something into d0
+    move.w    d0,d1          ; copy something into d1
+    swap      d0             ; swap d0
+    move.w    d1,d0          ; low word and high word of d0 now contain the same
+    and.l     d0,(a1)        ; apply mask to two consecutive words
+    move.l    (a0)+,d1       ; get next value from source data
+    or.l      d1,(a1)+
+    and.l     d0,(a1)+       ; write mask to positions 4 and 6
+    move.w    (a0)+,d1       ; get another value from source data
+    or.w      d1,-4(a1)
+    endm
+
 render_status_panel:
     move.w    #$5f,d7
 
 status_panel_line:
-    move.w    (a0)+,d0       ; load something into d0
-    move.w    d0,d1          ; copy something into d1
-    swap      d0             ; swap d0
-    move.w    d1,d0          ; low word and high word of d0 now contain the same
-    and.l     d0,(a1)        ; apply mask to two consecutive words
-    move.l    (a0)+,d1       ; get next value from source data
-    or.l      d1,(a1)+
-    and.l     d0,(a1)+       ; write mask to positions 4 and 6
-    move.w    (a0)+,d1       ; get another value from source data
-    or.w      d1,-4(a1)
+    status_panel_cpu_blit
 
     adda.w    #$80,a1
 
-    move.w    (a0)+,d0       ; load something into d0
-    move.w    d0,d1          ; copy something into d1
-    swap      d0             ; swap d0
-    move.w    d1,d0          ; low word and high word of d0 now contain the same
-    and.l     d0,(a1)        ; apply mask to two consecutive words
-    move.l    (a0)+,d1       ; get next value from source data
-    or.l      d1,(a1)+
-    and.l     d0,(a1)+       ; write mask to positions 4 and 6
-    move.w    (a0)+,d1       ; get another value from source data
-    or.w      d1,-4(a1)
-
-    move.w    (a0)+,d0       ; load something into d0
-    move.w    d0,d1          ; copy something into d1
-    swap      d0             ; swap d0
-    move.w    d1,d0          ; low word and high word of d0 now contain the same
-    and.l     d0,(a1)        ; apply mask to two consecutive words
-    move.l    (a0)+,d1       ; get next value from source data
-    or.l      d1,(a1)+
-    and.l     d0,(a1)+       ; write mask to positions 4 and 6
-    move.w    (a0)+,d1       ; get another value from source data
-    or.w      d1,-4(a1)
-
-    move.w    (a0)+,d0       ; load something into d0
-    move.w    d0,d1          ; copy something into d1
-    swap      d0             ; swap d0
-    move.w    d1,d0          ; low word and high word of d0 now contain the same
-    and.l     d0,(a1)        ; apply mask to two consecutive words
-    move.l    (a0)+,d1       ; get next value from source data
-    or.l      d1,(a1)+
-    and.l     d0,(a1)+       ; write mask to positions 4 and 6
-    move.w    (a0)+,d1       ; get another value from source data
-    or.w      d1,-4(a1)
+    status_panel_cpu_blit
+    status_panel_cpu_blit
+    status_panel_cpu_blit
 
     dbra      d7,status_panel_line
     rts
