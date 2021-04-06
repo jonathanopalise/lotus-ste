b19da6fc99cd6e306026d899bc680bd7c095b0df
Jonathan Thomas <jonathan@opalise.co.uk>
2021-04-06 11:56:57 +0100

    boot sector message

--- src/boot_sector.s
+++ src/boot_sector.s
@@ -10,3 +10,37 @@
 	move.l		d0,(a0)+
     rts
 
+    dc.b		'                                                '
+
+	dc.b		'Lots '
+	dc.b		'Of '
+	dc.b		'The '
+	dc.b		'Untapped '
+	dc.b		'Software '
+
+	dc.b		'Enhancing '
+	dc.b		'STE '
+	dc.b		'Potential '
+	dc.b		'Restricted '
+	dc.b		'Immensely '
+	dc.b		'This '
+
+	dc.b		'Therefore '
+	dc.b		'Underpowered '
+	dc.b		'Racer '
+	dc.b		'But '
+	dc.b		'Our '
+
+	dc.b		'Code '
+	dc.b		'Has '
+	dc.b		'Amiga '
+	dc.b		'Lovers '
+	dc.b		'Looking '
+	dc.b		'Ever '
+	dc.b		'Nonplussed. '
+	dc.b		'Gentlemen, (start your) '
+	dc.b		'Engines!'
+
+    dc.b		'                                                '
+
+    dc.b		'You WILL copy this game!'
